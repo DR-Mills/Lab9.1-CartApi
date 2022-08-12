c73131fd90399578128944117ea90c727d12f7c5
Dustin Mills <98778591+DR-Mills@users.noreply.github.com>
2022-08-11 22:38:14 -0400

    CRUD of CRUD complete; all deliverables finished

--- src/main/java/cart/api/CartItemController.java
+++ src/main/java/cart/api/CartItemController.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import cart.api.model.CartItem;
+
+@CrossOrigin(origins = "https://gc-express-tester.surge.sh/")
 
 @RestController
 public class CartItemController {
@@ -17,16 +17,15 @@ public class CartItemController {
 	private CartItemRepository cartRepo;
 
 	// Create
-
 	@PostMapping("cart-items")
 	@ResponseStatus(HttpStatus.CREATED)
 	public CartItem addCartItem(@RequestBody CartItem cartItem) {
 		cartRepo.insert(cartItem);
 		return cartItem;
 	}
+	
 
-	// Read - get cart items
-
+	// read all
 	@GetMapping("/cart-items")
 	public List<CartItem> readAll(@RequestParam(required = false) String product,
 			@RequestParam(required = false) Double maxPrice, @RequestParam(required = false) String prefix,
@@ -49,14 +48,27 @@ public class CartItemController {
 		}
 	}
 
-	
+	// read one
 	@GetMapping("/cart-items/{id}")
-	public CartItem getCartItem(@PathVariable String id) {
+	public CartItem getCartItem(@PathVariable("id") String id) {
 		return cartRepo.findById(id).orElseThrow(() -> new ItemNotFoundException(id));
 	}
-
-	// Update
 	
+	
+	// read total-cost in cart
+	@GetMapping("/cart-items/total-cost")
+	public Double getCartTotal() {
+		Double total = 0.00;
+		for (CartItem item : cartRepo.findAll()) {
+			total += (item.getPrice() * item.getQuantity());
+		}
+		total *= 1.06;
+		
+		return total;
+	}
+
+	
+	// Update
 	@PutMapping("/cart-items/{id}")
 	public CartItem updateCartItem(@PathVariable("id") String id, @RequestBody CartItem cartItem) {
 		cartRepo.findById(id).orElseThrow(() -> new ItemNotFoundException(id));	
@@ -66,13 +78,41 @@ public class CartItemController {
 	}
 
 	
+	// Patch
+	// increase quantity by 1
+	@PatchMapping("/cart-items/{id}/add")
+	public CartItem addQuantity(@PathVariable("id") String id) {
+		CartItem itemToUpdate = cartRepo.findById(id).orElseThrow(() -> new ItemNotFoundException(id));
+		itemToUpdate.setQuantity(itemToUpdate.getQuantity() + 1);
+		cartRepo.save(itemToUpdate);
+		return itemToUpdate;
+	}
+	
+	// decrease quantity by 1
+	@PatchMapping("/cart-items/{id}/minus")
+	public CartItem minusQuantity(@PathVariable("id") String id) {
+		CartItem itemToUpdate = cartRepo.findById(id).orElseThrow(() -> new ItemNotFoundException(id));
+		itemToUpdate.setQuantity(itemToUpdate.getQuantity() - 1);
+		cartRepo.save(itemToUpdate);
+		return itemToUpdate;
+	}
 	
 	
+	// Delete
+	@DeleteMapping("/cart-items/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteCartItem(@PathVariable("id") String id) {
+		cartRepo.deleteById(id);
+	}
+	
+	
+	// error handling for ItemNotFoundException
 	@ResponseBody
 	@ExceptionHandler(ItemNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	String itemNotFoundHandler(ItemNotFoundException ex) {
 		return ex.getMessage();
 	}
+
 
 }
